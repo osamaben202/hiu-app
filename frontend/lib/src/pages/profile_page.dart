350ebe757bde61193b03f00646e72d3c44294baa
osamaben202 <osamaben202@users.noreply.github.com>
2026-05-13 10:58:46 +0800

    fix: add missing ApiService import in profile_page.dart

--- frontend/lib/src/pages/profile_page.dart
+++ frontend/lib/src/pages/profile_page.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:hiu_app/src/services/api_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../providers/user_provider.dart';
 import 'login_page.dart';
